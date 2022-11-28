8997f60d3eb9ab48ce9020b3eb487d94ab6d31d9
unknown <osama_magdy0@yahoo.com>
2022-11-28 06:08:13 +0200

    the last form Home labtop

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const starsEl = document.querySelectorAll(".fa-star");
 const spaceEl = document.querySelectorAll(".space");
 
-// console.log(spaceEl);
-
 starsEl.forEach((star, index) => {
   star.onclick = () => {
-    // spaceEl[index].style.transform = `translateX(-${50 * index}px)`;
     starsEl.forEach((st) => {
-      // console.log(st);
       st.classList.remove("active");
     });
     for (let i = 0; i <= index; i++) {
-      // console.log(index);
-      // if(starsEl[i]>index)
       starsEl[i].classList.add("active");
     }
+
+    spaceEl.forEach((emo) => {
+      emo.style.transform = `translateX(-${index * 50}px)`;
+    });
   };
 });
